489f9cc3427a5aa96d4b723faad2ca2723a63f02
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-06-01 13:54:26 +0900

    Projections
    엔티티 대신에 DTO를 편리하게 조회할 때 사용
    전체 엔티티가 아니라 만약 회원 이름만 딱 조회하고 싶은 경우 등

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -66,4 +66,6 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Member findLockByUsername(String username);
+
+    List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -334,7 +334,6 @@ class MemberRepositoryTest {
 
         Member m1 = new Member("m1", 0 , teamA);
         Member m2 = new Member("m2", 0, teamA);
-        m1.setTeam(teamA);
         entityManager.persist(m1);
         entityManager.persist(m2);
 
@@ -353,6 +352,29 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findAll(example);
 
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
+
+    }
+
+    @Test
+    public void projections(){
+        //given
+        Team teamA = new Team("teamA");
+        entityManager.persist(teamA);
+
+        Member m1 = new Member("m1", 0 , teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        entityManager.persist(m1);
+        entityManager.persist(m2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        //when
+        List<UsernameOnly> result = memberRepository.findProjectionsByUsername("m1");
+
+        for(UsernameOnly usernameOnly : result){
+            System.out.println("usernameOnly = " + usernameOnly.getUsername());
+        }
 
     }
 }
